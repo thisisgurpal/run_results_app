ee7669d16396f02919e38843cf87c41bbd311c5e
Gurpal Gohler <gurpal.gohler@hotmail.co.uk>
2022-03-23 21:32:46 +0000

    first deploy heroku

--- project/settings.py
+++ project/settings.py
@@ -15,6 +15,10 @@ import os
 import django_on_heroku
 from dotenv import load_dotenv
 load_dotenv()
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -97,8 +101,8 @@ DATABASES = {
         'NAME': 'run_results_app',
         'HOST': 'localhost',
         'PORT': 5432,
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
     }
 }
 
