d0d4bc89f984fca76e1b0a7df0bfcd698802a4ea
Gurpal Gohler <gurpal.gohler@hotmail.co.uk>
2022-03-23 20:13:09 +0000

    first deploy try 2

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 import os
 import django_on_heroku
 from dotenv import load_dotenv
